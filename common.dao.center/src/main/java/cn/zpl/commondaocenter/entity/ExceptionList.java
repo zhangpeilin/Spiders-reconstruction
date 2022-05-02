84290c38733534b439756bcac79ad8f0ead673e0
zhangpeilin <512239520@163.com>
2022-05-02 22:54:47 +0800

    增加对新表的mvc

---
+++ common.dao.center/src/main/java/cn/zpl/commondaocenter/entity/ExceptionList.java
@@ -0,0 +1,38 @@
+package cn.zpl.commondaocenter.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author zpl
+ * @since 2022-05-02
+ */
+@TableName("exception_list")
+@ApiModel(value = "ExceptionList对象", description = "")
+public class ExceptionList implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String videoId;
+
+    public String getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(String videoId) {
+        this.videoId = videoId;
+    }
+
+    @Override
+    public String toString() {
+        return "ExceptionList{" +
+            "videoId=" + videoId +
+        "}";
+    }
+}
---
+++ common.dao.center/src/main/java/cn/zpl/commondaocenter/service/IExceptionListService.java
@@ -0,0 +1,16 @@
+package cn.zpl.commondaocenter.service;
+
+import cn.zpl.commondaocenter.entity.ExceptionList;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zpl
+ * @since 2022-05-02
+ */
+public interface IExceptionListService extends IService<ExceptionList> {
+
+}
