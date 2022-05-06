0674237c50b77d3a6bc68354fbcd161c88db4010
zhangpeilin <512239520@163.com>
2022-05-07 04:13:32 +0800

    controller增加新响应路径

--- common.dao.center/src/main/java/cn/zpl/commondaocenter/controller/ExceptionListController.java
+++ common.dao.center/src/main/java/cn/zpl/commondaocenter/controller/ExceptionListController.java
@@ -5,12 +5,12 @@ import cn.zpl.common.bean.RestResponse;
 import cn.zpl.commondaocenter.service.IExceptionListService;
 import cn.zpl.commondaocenter.utils.CommonUtils;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -22,7 +22,7 @@ import javax.annotation.Resource;
  * @author zpl
  * @since 2022-05-02
  */
-@Controller
+@RestController
 @RequestMapping("/commondaocenter/exceptionList")
 public class ExceptionListController {
 
@@ -30,12 +30,13 @@ public class ExceptionListController {
     IExceptionListService exceptionListService;
 
     @GetMapping("/get/{id}")
-    public String getById(@PathVariable("id") String id){
+    public RestResponse getById(@PathVariable("id") String id){
         QueryWrapper<ExceptionList> queryWrapper = new QueryWrapper<>();
         QueryWrapper<ExceptionList> video_id = queryWrapper.eq("video_id", id);
         ExceptionList one = exceptionListService.getOne(video_id);
+        video_id.getEntityClass();
         System.out.println(one);
-        return one.getVideoId();
+        return RestResponse.fail().item(null);
     }
 
     @PostMapping("/saveOrUpdateExceptionList")
