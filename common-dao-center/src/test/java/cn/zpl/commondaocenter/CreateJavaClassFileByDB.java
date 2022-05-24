7736f15a9b591b9d0de61128f451be6ffbe9b9fe
zhangpeilin <512239520@163.com>
2022-05-24 14:39:32 +0800

    新增图片分析controller

--- common-dao-center/src/test/java/cn/zpl/commondaocenter/CreateJavaClassFileByDB.java
+++ common-dao-center/src/test/java/cn/zpl/commondaocenter/CreateJavaClassFileByDB.java
@@ -14,15 +14,15 @@ public class CreateJavaClassFileByDB {
                     builder.author("zpl") // 设置作者
                             .enableSwagger() // 开启 swagger 模式
                             .fileOverride() // 覆盖已生成文件
-                            .outputDir("e://exception_list"); // 指定输出目录
+                            .outputDir("e://picture_analyze"); // 指定输出目录
                 })
                 .packageConfig(builder -> {
                     builder.parent("cn.zpl") // 设置父包名
                             .moduleName("commondaocenter") // 设置父包模块名
-                            .pathInfo(Collections.singletonMap(OutputFile.mapper, "e://exception_list")); // 设置mapperXml生成路径
+                            .pathInfo(Collections.singletonMap(OutputFile.mapper, "e://picture_analyze")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
-                    builder.addInclude("exception_list") // 设置需要生成的表名
+                    builder.addInclude("picture_analyze") // 设置需要生成的表名
                             .addTablePrefix("t_", "c_"); // 设置过滤表前缀
                 })
                 .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
