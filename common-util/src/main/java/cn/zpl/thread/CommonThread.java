cb4ab133b2950cedcbefdd5e21b6fc190d70055c
zhangpeilin <512239520@163.com>
2022-05-24 17:42:45 +0800

    公共模块增加配置

--- common-util/src/main/java/cn/zpl/thread/CommonThread.java
+++ common-util/src/main/java/cn/zpl/thread/CommonThread.java
@@ -1,16 +1,24 @@
 package cn.zpl.thread;
 
 
+import cn.zpl.pojo.DoRetry;
 import cn.zpl.pojo.DownloadDTO;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Vector;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public abstract class CommonThread implements Runnable {
 
 
     Vector<DownloadDTO> dtoList = new Vector<>();
+
+    public DoRetry getDoRetry() {
+        return doRetry;
+    }
+
+    DoRetry doRetry = new DoRetry();
     DownloadDTO dto = new DownloadDTO();
     public String url;
 
@@ -35,18 +43,29 @@ public abstract class CommonThread implements Runnable {
         try {
             domain();
         } catch (Exception e) {
-            log.error("执行出错：\n", e);
-            log.error("5秒后重试");
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
+            if (doRetry()) {
+                log.error("执行出错：\n", e);
+                log.error("2秒后重试");
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                log.debug("准备重试");
+                run();
             }
-            log.debug("准备重试");
-            run();
         }
     }
 
     public abstract void domain();
 
+    public boolean doRetry(){
+        if (doRetry.canDoRetry()) {
+            doRetry.doRetry();
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
