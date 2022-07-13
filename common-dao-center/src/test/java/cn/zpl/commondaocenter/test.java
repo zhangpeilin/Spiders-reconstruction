f20496352b9df9df75e894a1d9879e60d1c91d55
zhangpl1 <zhangpl1@yonyou.com>
2022-07-13 16:18:00 +0800

    [common-dao-center]test2

---
+++ common-dao-center/src/test/java/cn/zpl/commondaocenter/test.java
@@ -0,0 +1,14 @@
+package cn.zpl.commondaocenter;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ *
+ * @author zhangpl1
+ * @date 2022/7/13
+ */
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
