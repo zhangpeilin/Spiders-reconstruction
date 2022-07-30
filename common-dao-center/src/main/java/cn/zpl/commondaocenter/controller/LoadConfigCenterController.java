18d76a69e547959a203de14da187a69f34ba598d
zhangpeilin <512239520@163.com>
2022-07-31 01:11:04 +0800

    [commondaocenter]读取配置中心服务

---
+++ common-dao-center/src/main/java/cn/zpl/commondaocenter/controller/LoadConfigCenterController.java
@@ -0,0 +1,17 @@
+package cn.zpl.commondaocenter.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoadConfigCenterController {
+
+    @Value("${spring.datasource.url}")
+    String info;
+
+    @GetMapping("/loadConfig")
+    public String loadConfig() {
+        return info;
+    }
+}
