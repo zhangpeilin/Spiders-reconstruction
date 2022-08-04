d23b7e487b5e8fbbee155ef70fef45b31afd502e
zhangpeilin <512239520@163.com>
2022-08-04 14:28:54 +0800

    [spider-on-bika]配置类

--- spider-on-bika/src/main/java/cn/zpl/spider/on/bika/common/BikaParams.java
+++ spider-on-bika/src/main/java/cn/zpl/spider/on/bika/common/BikaParams.java
@@ -1,8 +1,18 @@
 package cn.zpl.spider.on.bika.common;
 
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "spider.bika")
 public class BikaParams {
 
     public static boolean writeDB = false;
     public static boolean isForceDownload = false;
+
+    private String email;
+    private String password;
 }
