b0e491e257cb1426ea98ec4671feda25a799eeba
zhangpeilin <512239520@163.com>
2022-08-04 14:38:24 +0800

    [spider-on-bika]更新配置文件

---
+++ spider-on-bika/src/main/java/cn/zpl/spider/on/bika/BikaApplication.java
@@ -0,0 +1,18 @@
+package cn.zpl.spider.on.bika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ *
+ * @author zhangpl1
+ * @date 2022/8/4
+ */
+@SpringBootApplication
+public class BikaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BikaApplication.class, args);
+    }
+}
---
+++ spider-on-bika/src/test/java/cn/zpl/spider/on/bika/ApplicationConfigTest.java
@@ -0,0 +1,25 @@
+package cn.zpl.spider.on.bika;
+
+import cn.zpl.spider.on.bika.common.BikaParams;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ *
+ * @author zhangpl1
+ * @date 2022/8/4
+ */
+@SpringBootTest
+public class ApplicationConfigTest {
+
+    @Resource
+    BikaParams params;
+    @Test
+    public void loadConfig() {
+        System.out.println(params.getEmail());
+    }
+}
