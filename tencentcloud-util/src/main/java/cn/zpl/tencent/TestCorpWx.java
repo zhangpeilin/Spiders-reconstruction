aae1c2930f91690ef55c32a391e3aad3bab7bc5d
zhangpeilin <512239520@163.com>
2022-08-23 17:03:51 +0800

    [spider-on-tencent]使用自动任务触发检测

--- tencentcloud-util/src/main/java/cn/zpl/tencent/TestCorpWx.java
+++ tencentcloud-util/src/main/java/cn/zpl/tencent/TestCorpWx.java
@@ -1,6 +1,7 @@
 package cn.zpl.tencent;
 
 import cn.zpl.tencent.common.TencentParams;
+import cn.zpl.tencent.config.TencentConfig;
 import cn.zpl.util.CommonIOUtils;
 import com.google.gson.JsonElement;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -16,14 +18,14 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
-@SpringBootTest
+@Component
 public class TestCorpWx {
 
     @Resource
     TencentParams tencentParams;
 
-    @org.junit.jupiter.api.Test
-    public void main() {
+    public void doBusiness() {
+
         WeChatMsgSend swx = new WeChatMsgSend();
         try {
             RestTemplate restTemplate = new RestTemplate();
@@ -36,35 +38,37 @@ public class TestCorpWx {
             String format = dateFormat.format(instance.getTime());
             int hour = instance.get(Calendar.HOUR_OF_DAY);
             String postdata = null;
-            while (hour == 8) {
-                hour = instance.get(Calendar.HOUR_OF_DAY);
-                ResponseEntity<String> exchange = restTemplate.exchange("http://www.szse.cn/api/report/ShowReport/data?SHOWTYPE=JSON&CATALOGID=option_hybg&loading=first", HttpMethod.GET, new HttpEntity<String>(headers), String.class);
-                JsonElement jsonElement = CommonIOUtils.paraseJsonFromStr(exchange.getBody());
-                if (jsonElement.isJsonArray()) {
-                        JsonElement element = jsonElement.getAsJsonArray().get(0);
-                        JsonElement data = CommonIOUtils.getFromJson2(element, "data");
-                        JsonElement conditions = CommonIOUtils.getFromJson2(element, "metadata-conditions");
-                        if (conditions.isJsonArray()) {
-                            String defaultValue = conditions.getAsJsonArray().get(0).getAsJsonObject().get("defaultValue").getAsString();
-                            if (defaultValue.equals(format)) {
-                                postdata = swx.createpostdata(tencentParams.getToUser(), "text", tencentParams.getApplicationId(), "content", "网站更新了");
-                                break;
-                            }
+            hour = instance.get(Calendar.HOUR_OF_DAY);
+            ResponseEntity<String> exchange = restTemplate.exchange("http://www.szse.cn/api/report/ShowReport/data?SHOWTYPE=JSON&CATALOGID=option_hybg&loading=first", HttpMethod.GET, new HttpEntity<String>(headers), String.class);
+            JsonElement jsonElement = CommonIOUtils.paraseJsonFromStr(exchange.getBody());
+            if (jsonElement.isJsonArray()) {
+                JsonElement element = jsonElement.getAsJsonArray().get(0);
+                JsonElement data = CommonIOUtils.getFromJson2(element, "data");
+                JsonElement conditions = CommonIOUtils.getFromJson2(element, "metadata-conditions");
+                if (conditions.isJsonArray()) {
+                    String defaultValue = conditions.getAsJsonArray().get(0).getAsJsonObject().get("defaultValue").getAsString();
+                    if (defaultValue.equals(format)) {
+                        postdata = swx.createpostdata(tencentParams.getToUser(), "text", tencentParams.getApplicationId(), "content", "网站更新了");
+                    }
 //                        if (data.isJsonArray() && data.getAsJsonArray().size() == 0 && ) {
 //                            postdata = swx.createpostdata("@all", "text", tencentParams.getApplicationId(), "content","网站更新了");
 //                        } else {
 //                            postdata = swx.createpostdata("@all", "text", tencentParams.getApplicationId(), "content","这是一条测试信息");
 //                        }
-                        }
                 }
-
-                TimeUnit.SECONDS.sleep(10);
             }
             if (postdata == null) {
                 return;
             }
             String token = swx.getToken(tencentParams.getCorpId(), tencentParams.getCorpSecret());
+            swx.post("utf-8", WeChatMsgSend.CONTENT_TYPE, (new WeChatUrlData()).getSendMessage_Url(), postdata, token);
+            TimeUnit.SECONDS.sleep(3);
+            postdata = swx.createpostdata(tencentParams.getToUser(), "text", tencentParams.getApplicationId(), "content", "记得吃药");
             String resp = swx.post("utf-8", WeChatMsgSend.CONTENT_TYPE, (new WeChatUrlData()).getSendMessage_Url(), postdata, token);
+            String errcode = CommonIOUtils.getFromJson2Str(CommonIOUtils.paraseJsonFromStr(resp), "errcode");
+            if ("0".equals(errcode)) {
+                TencentConfig.cache.put("isNotice", true);
+            }
             System.out.println("获取到的token======>" + token);
             System.out.println("请求数据======>" + postdata);
             System.out.println("发送微信的响应数据======>" + resp);
--- tencentcloud-util/src/main/java/cn/zpl/tencent/config/TencentConfig.java
+++ tencentcloud-util/src/main/java/cn/zpl/tencent/config/TencentConfig.java
@@ -1,10 +1,44 @@
 package cn.zpl.tencent.config;
 
+import cn.zpl.config.SpringContext;
+import cn.zpl.tencent.TestCorpWx;
 import cn.zpl.tencent.common.TencentParams;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import lombok.SneakyThrows;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableConfigurationProperties(TencentParams.class)
+@EnableScheduling
 public class TencentConfig {
+
+    public static LoadingCache<String, Boolean> cache = CacheBuilder.newBuilder().maximumSize(1).expireAfterWrite(2, TimeUnit.HOURS).build(new CacheLoader<String, Boolean>() {
+        @Override
+        public @NotNull Boolean load(@NotNull String key) {
+            return false;
+        }
+    });
+
+    //每天8:30开始每5分钟执行一次
+    @SneakyThrows
+    @Scheduled(cron = "0 30/5 8 * * *")
+//    @Scheduled(cron = "0/5 * 16 * * *")
+    private void configureTask(){
+        if (cache.get("isNotice")) {
+            return;
+        }
+        TestCorpWx TestCorpWx = (TestCorpWx) SpringContext.getBeanWithGenerics(TestCorpWx.class);
+        TestCorpWx.doBusiness();
+        //判断当天是否已经通知过，如果通知过则跳过执行
+        System.out.println("自动任务执行时间：" + LocalDateTime.now());
+    }
 }
