6e8f983f3bfd7ed2dd33be79417b25bdae060a71
zhangpeilin <512239520@163.com>
2022-09-16 16:23:28 +0800

    [spider-on-bilibili-manga]从配置中心读取参数

--- spider-on-bilibili-manga/src/main/java/cn/zpl/spider/on/bilibili/manga/util/BilibiliMangaProperties.java
+++ spider-on-bilibili-manga/src/main/java/cn/zpl/spider/on/bilibili/manga/util/BilibiliMangaProperties.java
@@ -1,12 +1,14 @@
 package cn.zpl.spider.on.bilibili.manga.util;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
 @ConfigurationProperties(prefix = "spider.bilibili.manga")
 public class BilibiliMangaProperties {
 
+    @Value("${spider.bilibili.cookies}")
     public String bilibiliCookies;
     public String commonHeaders;
     public String getComicDetailUrl;
