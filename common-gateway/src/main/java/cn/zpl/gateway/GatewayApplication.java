9a1faf06d4a977cdc51324b7b02fea100cb2f7b3
zhangpeilin <512239520@163.com>
2022-09-19 22:56:03 +0800

    新增注册中心

---
+++ common-gateway/src/main/java/cn/zpl/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package cn.zpl.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ *
+ * @author zhangpl1
+ * @date 2022/9/14
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
