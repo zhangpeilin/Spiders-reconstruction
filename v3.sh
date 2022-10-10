dcbc6d3b59fbb87eadf9187dbaa8a0149112cb27
zhangpeilin <512239520@163.com>
2022-10-11 00:56:42 +0800

    [Spiders-reconstruction]修改docker内部地址

--- v3.sh
+++ v3.sh
@@ -52,11 +52,11 @@ echo "打包镜像"
 docker build -t "$MODULE" --target "$MODULE" .
 
 case $MODULE in
-common-gateway) echo "启动网关容器" && docker run -d -p 8089:8089 --ip 172.17.0.6 --name common-gateway common-gateway
+common-gateway) echo "启动网关容器" && docker run -d -p 8089:8089 --ip 172.18.0.6 --name common-gateway common-gateway
   ;;
 common-config-center) echo "启动配置中心" && docker run -d -p 3344:3344 --name common-config-center common-config-center
   ;;
 common-dao-center) echo "启动数据库服务" && docker run -d -p 8087:8087 --name common-dao-center common-dao-center
   ;;
-common-app-eureka) echo "启动注册中心" && docker run -d -p 7001:7001 --ip 172.17.0.4 --name common-app-eureka common-app-eureka
+common-app-eureka) echo "启动注册中心" && docker run -d -p 7001:7001 --ip 172.18.0.4 --name common-app-eureka common-app-eureka
 esac
