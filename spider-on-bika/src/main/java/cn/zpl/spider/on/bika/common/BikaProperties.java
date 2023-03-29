81fccb15cea7448e863b37c0a33b3c7feab340a8
zhangpeilin <512239520@163.com>
2023-03-29 11:15:01 +0800

    [spider-on-bika]工具类新增方法

--- spider-on-bika/src/main/java/cn/zpl/spider/on/bika/common/BikaProperties.java
+++ spider-on-bika/src/main/java/cn/zpl/spider/on/bika/common/BikaProperties.java
@@ -3,17 +3,20 @@ package cn.zpl.spider.on.bika.common;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+/**
+ * @author zpl
+ */
 @Data
 @ConfigurationProperties(prefix = "spider.bika")
-public class BikaParams {
+public class BikaProperties {
 
     public static boolean isForceDownload = true;
     private String email;
     private String password;
     private List<String> savePath;
     private boolean writeDB = true;
+    private String keywords;
 }
