a9cfbc662fe04d7c1ded8906c632a3c8bdc2b3af
zhangpeilin <512239520@163.com>
2023-08-09 16:03:21 +0800

    [spider-dao]新增实体类

---
+++ spider-dao/src/main/java/cn/zpl/dao/mapper/ServerInfoMapper.java
@@ -0,0 +1,16 @@
+package cn.zpl.dao.mapper;
+
+import cn.zpl.common.bean.ServerInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 记录家用服务器ip Mapper 接口
+ * </p>
+ *
+ * @author zpl
+ * @since 2023-07-20
+ */
+public interface ServerInfoMapper extends BaseMapper<ServerInfo> {
+
+}
